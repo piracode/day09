13b4141f421157300d78892444b7188b1cb4bc60
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2024-11-06 11:10:03 -0800

    Added menu toggle option 1

--- scripts/menu-toggle.js
+++ scripts/menu-toggle.js
@@ -1,4 +1,10 @@
 // Main Navigation Toggle
+const button = document.getElementById("main-nav-button");
+const links = document.getElementById("main-nav-list");
 
+button.addEventListener("click", function() {
+    links.classList.toggle("toggled");
+    console.log("click");
+})
 
 // Thumb Navigation Toggle
